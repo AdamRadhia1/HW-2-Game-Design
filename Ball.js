1dacfd0ddd30c2d8627795d84a026cf031ee43fb
AdamRadhia1 <radhia.adam@gwu.edu>
2025-10-30 18:28:40 -0400

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -4,17 +4,25 @@ class Ball {
     this.radius = 34;
     this.bounceVel = 0;
     this.gravity = 0.5;
+
     this.milestoneText = "";
     this.milestoneTimer = 0;
   }
 
-  update() {
+  update(road) {
+    // follow mouse horizontally
     this.pos.x = lerp(this.pos.x, mouseX, 0.2);
+
+    // constrain to road
+    this.pos.x = constrain(this.pos.x, road.leftGutterX, road.rightGutterX);
+
+    // gentle vertical bounce resolution (as in your Processing)
     this.pos.y += this.bounceVel;
     this.bounceVel += this.gravity;
 
-    if (this.pos.y > height - 100) {
-      this.pos.y = height - 100;
+    const floorY = height - 100;
+    if (this.pos.y > floorY) {
+      this.pos.y = floorY;
       this.bounceVel = 0;
     }
 
@@ -24,41 +32,52 @@ class Ball {
   display() {
     push();
 
-    let glow = map(nightFade,0,1,0,80);
-    fill(255, glow);
-    noStroke();
-    circle(this.pos.x, this.pos.y, this.radius*1.9);
+    // night glow aura
+    const glow = map(nightFade, 0, 1, 0, 80);
+    noStroke(); fill(255, glow);
+    circle(this.pos.x, this.pos.y, this.radius * 1.9);
 
+    // ghost body (same silhouette as your PDE)
     fill(255);
-    let w = this.radius*1.2, h = this.radius*1.5;
-    circle(this.pos.x, this.pos.y-h*0.25, w);
+    const w = this.radius * 1.2;
+    const h = this.radius * 1.5;
+
+    circle(this.pos.x, this.pos.y - h * 0.25, w); // head
     rectMode(CENTER);
-    rect(this.pos.x,this.pos.y+h*0.05,w,h*0.6,10);
+    rect(this.pos.x, this.pos.y + h * 0.05, w, h * 0.6, 10); // torso
 
-    let seg = w/5;
-    for (let i=0;i<5;i++)
-      circle(this.pos.x-w/2+seg/2+i*seg,this.pos.y+h*0.35,seg*0.6);
+    // teeth / frill
+    const teeth = 5;
+    const seg = w / teeth;
+    for (let i = 0; i < teeth; i++) {
+      circle(this.pos.x - w/2 + seg/2 + i*seg, this.pos.y + h*0.35, seg*0.6);
+    }
 
+    // eyes + mouth
     fill(30);
-    circle(this.pos.x-w*0.2,this.pos.y-h*0.25,w*0.15);
-    circle(this.pos.x+w*0.2,this.pos.y-h*0.25,w*0.15);
+    circle(this.pos.x - w*0.2, this.pos.y - h*0.25, w*0.15);
+    circle(this.pos.x + w*0.2, this.pos.y - h*0.25, w*0.15);
 
     noFill(); stroke(30); strokeWeight(3);
-    arc(this.pos.x,this.pos.y-h*0.05,w*0.3,w*0.2,0,PI);
+    arc(this.pos.x, this.pos.y - h*0.05, w*0.3, w*0.2, 0, PI);
 
     pop();
 
+    // milestone popup
     if (this.milestoneTimer > 0) {
-      fill(255,230,90);
+      fill(255, 230, 90);
+      noStroke();
       textAlign(CENTER);
       textSize(20);
-      text(this.milestoneText,this.pos.x,this.pos.y-this.radius-18);
+      text(this.milestoneText, this.pos.x, this.pos.y - this.radius - 18);
     }
   }
+
+  bounce() { this.bounceVel = -10; }
+  resetBounce() { this.bounceVel = 0; }
 
   showMilestone(msg) {
     this.milestoneText = msg;
     this.milestoneTimer = 60;
   }
 }
-
