26ccc5f39b02110f47c83ef57231bce1ebfa3c2b
AdamRadhia1 <radhia.adam@gwu.edu>
2025-10-30 20:40:11 -0400

    Update Road.js

--- Road.js
+++ Road.js
@@ -1,29 +1,22 @@
 class Road {
   constructor() {
-    this.roadWidth = width / 3;
-    this.margin = (width - this.roadWidth) / 2;
+    this.roadWidth   = width / 3;
+    this.margin      = (width - this.roadWidth) / 2;
     this.leftGutterX = this.margin;
-    this.rightGutterX = this.margin + this.roadWidth;
+    this.rightGutterX= this.margin + this.roadWidth;
     this.stripeOffsetY = 0;
 
     this.houses = [];
-    this.clouds = [];
-
     let y = 0;
     for (let i = 0; i < 12; i++) {
-      this.houses.push(new House(true, y, this.margin));
-      this.houses.push(new House(false, y, this.margin));
+      this.houses.push(new House(true,  y, this.roadWidth));
+      this.houses.push(new House(false, y, this.roadWidth));
       y += 120;
-    }
-
-    // add clouds
-    for (let i = 0; i < 5; i++) {
-      this.clouds.push(new Cloud(random(width), random(50,200), random(0.6,1.2)));
     }
   }
 
-  update() {
-    let s = max(5, baseSpeed + distance / 300);
+  update(darkMode) {
+    const s = max(5, baseSpeed + distance / 300.0);
     this.stripeOffsetY = (this.stripeOffsetY + s) % 40;
 
     for (let h of this.houses) {
@@ -32,43 +25,63 @@ class Road {
       h.updateBubble();
     }
 
-    for (let c of this.clouds) c.update();
-
-    let d = lerp(1,0.25, nightFade);
-    this.roadColor = color(250*d,199*d,179*d);
+    const darkness = lerp(1, 0.2, nightFade);
+    this.roadColor = color(250 * darkness, 199 * darkness, 179 * darkness);
   }
 
   display() {
-    // sky
-    background(170 * (1-nightFade), 150 * (1-nightFade), 200);
-
-    // clouds
-    for (let c of this.clouds) c.display();
-
-    // houses behind road
-    for (let h of this.houses) h.display(false);
-
     // road
-    fill(this.roadColor);
-    noStroke();
+    noStroke(); fill(this.roadColor);
     rect(this.margin, 0, this.roadWidth, height);
 
-    stroke(255);
-    strokeWeight(3);
-    for (let y = int(this.stripeOffsetY); y < height; y += 40)
-      line(width / 2, y, width / 2, y + 20);
+    // center stripes
+    stroke(255); strokeWeight(3);
+    for (let y = int(this.stripeOffsetY); y < height; y += 40) {
+      line(width/2, y, width/2, y + 20);
+    }
 
-    // houses bubbles on top
-    for (let h of this.houses) h.display(true);
+    // houses (under bubbles)
+    for (let h of this.houses) h.display(false);
+
+    // collect bubbles, prevent overlaps per side
+    const bubbleItems = [];
+    for (let h of this.houses) if (h.showBubble) {
+      const tw = textWidth(h.msg) + 16;
+      const bx = h.x + h.w/2 - tw/2;
+      const by = h.y - 35;
+      bubbleItems.push({ house:h, x:bx, y:by, w:tw, h:24, left:h.leftSide });
+    }
+
+    // sort by y so we accept the first, skip the next if overlapping
+    bubbleItems.sort((a,b)=> a.y - b.y);
+
+    const drawnLeft  = [];
+    const drawnRight = [];
+
+    for (let item of bubbleItems) {
+      const arr = item.left ? drawnLeft : drawnRight;
+      let overlaps = false;
+      for (let d of arr) {
+        if (abs(item.y - d.y) < 28) { overlaps = true; break; }
+      }
+      if (!overlaps) {
+        item.house.drawBubbleCrisp();
+        arr.push({ y:item.y });
+      }
+    }
   }
 }
 
-// 🏠 House
 class House {
-  constructor(leftSide, startY, marginRef) {
-    this.leftSide = leftSide;
-    this.y = startY;
-    this.marginRef = marginRef;
+  constructor(leftSide, startY, roadWidth) {
+    this.leftSide  = leftSide;
+    this.y         = startY;
+    this.roadWidth = roadWidth;
+
+    this.showBubble = false;
+    this.msg = "";
+    this.timer = 0;
+
     this.randomize();
     this.position();
   }
@@ -76,20 +89,21 @@ class House {
   randomize() {
     this.w = random(50, 80);
     this.h = random(60, 110);
-    this.body = color(random(180,255), random(120,255), random(120,255));
-    this.roof = lerpColor(this.body, color(0), 0.35);
+    this.body   = color(random(180,255), random(120,255), random(120,255));
+    this.roof   = lerpColor(this.body, color(0), 0.35);
     this.window = color(255, 255, random(110,210));
   }
 
   position() {
-    let pad = 18;
+    const pad = 10;
+    const margin = (width - this.roadWidth) / 2;
     this.x = this.leftSide
-      ? random(pad, this.marginRef - this.w - pad)
-      : random(width - this.marginRef + pad, width - this.w - pad);
+      ? random(pad, margin - this.w - pad)
+      : random(width - margin + pad, width - this.w - pad);
   }
 
-  reset(y) {
-    this.y = y;
+  reset(newY) {
+    this.y = newY;
     this.randomize();
     this.position();
     this.showBubble = false;
@@ -97,71 +111,62 @@ class House {
   }
 
   updateBubble() {
-    if (!this.showBubble && random() < 0.0018) {
-      let chatter = [
+    if (!this.showBubble && random(1) < 0.002) {
+      this.showBubble = true;
+      const chatter = [
         "Did you see that ghost!",
         "Someone catch that ghost!",
+        "Halloween is getting too scary!",
+        "No way that's a real ghost!",
+        "Mommy can I go pet the ghost?",
+        "Oh shoot!",
         "Not Halloween being Halloween!",
-        "911, I see a ghost outside!",
+        "911, I see a ghost!",
         "That ghost has moves!",
-        "Close the curtains!"
+        "Close the curtains!",
+        "Who you gonna call?!",
+        "I'm outta here!",
+        "That's one bold ghost!"
       ];
       this.msg = chatter[int(random(chatter.length))];
-      this.showBubble = true;
-      this.timer = 150;
-    } else if (this.showBubble) {
-      this.timer--;
-      if (this.timer <= 0) this.showBubble = false;
+      this.timer = int(random(120, 240));
     }
+    if (this.showBubble) this.timer--;
+    if (this.timer <= 0) this.showBubble = false;
   }
 
   display(bubblePass) {
     if (!bubblePass) {
-      fill(this.body);
-      noStroke();
+      // pastel flat house
+      noStroke(); fill(this.body);
       rect(this.x, this.y, this.w, this.h);
       fill(this.roof);
-      triangle(this.x, this.y, this.x + this.w/2, this.y-22, this.x + this.w, this.y);
-      fill(this.window);
-      rect(this.x + 12, this.y + 18, 15, 15, 4);
-      rect(this.x + this.w - 27, this.y + 18, 15, 15, 4);
-      rect(this.x + 12, this.y + this.h - 35, 15, 15, 4);
-      rect(this.x + this.w - 27, this.y + this.h - 35, 15, 15, 4);
-    } 
-    else if (this.showBubble) {
-      push();
-      textSize(12);
-      let tw = textWidth(this.msg) + 20;
-      fill(255);
-      stroke(0);
-      strokeWeight(2);
-      rect(this.x + this.w/2 - tw/2, this.y - 28, tw, 22, 12);
-      fill(0);
-      noStroke();
-      textAlign(CENTER,CENTER);
-      text(this.msg, this.x + this.w/2, this.y - 18);
-      pop();
-    }
-  }
-}
+      triangle(this.x, this.y, this.x + this.w/2, this.y - 25, this.x + this.w, this.y);
 
-// ☁️ Cloud
-class Cloud {
-  constructor(x,y,s){
-    this.x=x; this.y=y; this.s=s;
-  }
-  update(){
-    this.x += 0.3*this.s;
-    if(this.x>width+80){
-      this.x=-80;
-      this.y=random(50,250);
+      // windows (no flicker)
+      fill(this.window); noStroke();
+      const spacing = this.w/4;
+      rect(this.x + spacing,     this.y + this.h/3,     15, 15, 3);
+      rect(this.x + 2*spacing,   this.y + this.h/3,     15, 15, 3);
+      rect(this.x + spacing,     this.y + 2*this.h/3-8, 15, 15, 3);
+      rect(this.x + 2*spacing,   this.y + 2*this.h/3-8, 15, 15, 3);
     }
   }
-  display(){
-    noStroke();
-    fill(255,240);
-    ellipse(this.x,this.y,60,40);
-    ellipse(this.x+22,this.y+5,50,30);
-    ellipse(this.x-22,this.y+5,50,30);
+
+  drawBubbleCrisp() {
+    if (!this.showBubble) return;
+    // clean, thin bubble
+    const tw = textWidth(this.msg) + 16;
+    const bx = this.x + this.w/2 - tw/2;
+    const by = this.y - 35;
+
+    stroke(0, 140); strokeWeight(1);
+    fill(255, 245);
+    rect(bx, by, tw, 24, 10);
+
+    noStroke(); fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(12);
+    text(this.msg, this.x + this.w/2, this.y - 23);
   }
 }
